ed77dd3d7532f9a78465f9efbeacea4cfc0e1b47
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-04-15 04:12:07 -0500

    loader added
    Added CSS loader

--- script.js
+++ script.js
@@ -3,35 +3,55 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
-// Get quotes from API
+// Loading Spinner Shown
+function loading() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
+
+// Remove Loading Spinner
+function complete() {
+	quoteContainer.hidden = false;
+	loader.hidden = true;
+}
+
+// Show New Quote
+function newQuote() {
+	loading();
+	// Pick a random quote from array
+	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+	// Check if Author field is blank and replace it with 'Unknown'
+	if (!quote.author) {
+		authorText.textContent = 'Unknown';
+	} else {
+		authorText.textContent = quote.author;
+	}
+	// Check Quote length to determine styling
+	if (quote.text.length > 120) {
+		quoteText.classList.add('long-quote');
+	} else {
+		quoteText.classList.remove('long-quote');
+	}
+	// Set Quote, Hide Loader
+	quoteText.textContent = quote.text;
+	complete();
+}
+
+// Get Quotes From API
 async function getQuotes() {
+	loading();
 	const apiUrl = 'https://type.fit/api/quotes';
 	try {
 		const response = await fetch(apiUrl);
 		apiQuotes = await response.json();
 		newQuote();
 	} catch (error) {
-		// Catch error here
+		// Catch Error Here
 	}
-}
-
-// Show new quote
-function newQuote() {
-	// Pick a random quote from apiQuotes array
-	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-	// Check if author field is blank and replace with 'unknown'
-	!quote.author
-		? (authorText.textContent = 'Unknown')
-		: (authorText.textContent = quote.author);
-	quoteText.textContent = quote.text;
-
-	// check quote length
-	quote.text.length > 120
-		? quoteText.classList.add('long-quote')
-		: quoteText.classList.remove('long-quote');
 }
 
 // Tweet Quote
@@ -40,9 +60,9 @@ function tweetQuote() {
 	window.open(twitterUrl, '_blank');
 }
 
-// Event listeners
+// Event Listeners
 newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
-// On load
+// On Load
 getQuotes();
